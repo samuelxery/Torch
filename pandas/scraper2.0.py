d7f809d8210321e8313095a56f461028a2fa7c32
samuelxery <samuelfabian260311@gmail.com>
2026-07-01 15:57:55 -0400

    push attempt

--- pandas/scraper2.0.py
+++ pandas/scraper2.0.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from time import sleep
+import pandas as pd
 driver = webdriver.Chrome()
 scraped_data = []
 # main loop :p
@@ -8,18 +8,30 @@ for i in range(1, 51):
     site_url = f"https://books.toscrape.com/catalogue/page-{i}.html"
     driver.get(site_url)
     books = driver.find_elements(By.CLASS_NAME, "product_pod")
+    book_urls = []
     for book in books:
-        book_elements = book.find_element(By.TAG_NAME, "h3")
-        book_tags = book_elements.find_element(By.TAG_NAME, "a")
-        book_url = book_tags.get_attribute("href")
-        driver.get(book_url)
-        details = driver.find_elements(By.CLASS_NAME, "row")
+        element = book.find_element(By.CSS_SELECTOR, "h3 a")
+        book_urls.append(element.get_attribute("href"))
+        
+    for url in book_urls:
+        driver.get(url)
+        details = driver.find_elements(By.CLASS_NAME, "product_page")
+        main_details = driver.find_element(By.CSS_SELECTOR, "div.col-sm-6.product_main")
+        title = main_details.find_element(By.TAG_NAME, "h1").text
+        price = main_details.find_element(By.CLASS_NAME, "price_color").text
         for detail in details:
-            main_details = detail.find_element(By.CLASS_NAME, "col-sm-6 product-main")
-            title = main_details.find_element(By.TAG_NAME, "h1").text
-            price = main_details.find_element(By.CLASS_NAME, "price_color").text
+            try:
+                description = driver.find_element(By.XPATH, "//*[@id='content_inner']/article/p").text
+                upc = driver.find_element(By.XPATH, "//*[@id='content_inner']/article/table/tbody/tr[1]/td").text
+                genre = driver.find_element(By.XPATH, "//*[@id='default']/div/div/ul/li[3]/a").text
+            except:
+                continue
         scraped_data.append({
             "title": title,
-            "price": price
+            "price": price,
+            "description": description,
+            "UPC": upc,
+            "genre": genre
         })
-print(scraped_data)
+df = pd.DataFrame(scraped_data)
+df.to_json("bookss.jsonl", orient="records", lines=True)
